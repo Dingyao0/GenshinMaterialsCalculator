e4d8989181c0d9cb481ee5ad365ff05d51ca3c28
Dingyao0 <dingyao03@gmail.com>
2022-02-03 01:07:34 +0800

    database Rosaria

--- app/src/main/java/com/example/genshinmaterialscalculator/MainActivity.java
+++ app/src/main/java/com/example/genshinmaterialscalculator/MainActivity.java
@@ -144,7 +144,7 @@ public class MainActivity extends AppCompatActivity {
 
                         db.addCharacter(new Character("Rosaria", "Cryo", "Polearm", "1030", "20", "60", R.drawable.atk_pct, "0", "12",
                                 "12", R.drawable.rosaria, "rosaria", "4-star", "sdescriptionasd",
-                                " A sister of the church, though you wouldn't know it if it weren't for her attire. Known for her sharp, cold words and manner, she often works alone..", "regio", R.drawable.w_2313, R.drawable.w_2313, R.drawable.w_2313, R.drawable.rosariabanner,
+                                " A sister of the church, though you wouldn't know it if it weren't for her attire. Known for her sharp, cold words and manner, she often works alone..", "regio", R.drawable.spear_of_the_church, R.drawable.ravaging_confession, R.drawable.rites_of_termination, R.drawable.rosariabanner,
                                 1, 1, 1, 1, 1));
 
                         db.addCharacter(new Character("Sangonomiya Kokomi", "Hydro", "Catalyst", "1049", "18", "51", R.drawable.atk_pct, "0", "12",
@@ -164,6 +164,7 @@ public class MainActivity extends AppCompatActivity {
                         db.addCharacterLocal(new CharacterLocal("Dandelion Seed","Mondstadt", R.drawable.dandelion_seed));
                         db.addCharacterLocal(new CharacterLocal("Sil Flower", "Li Yue", R.drawable.silk_flower));
                         db.addCharacterLocal(new CharacterLocal("Onikabuto","Inazuma", R.drawable.onikabuto));
+                        db.addCharacterLocal(new CharacterLocal("Valberry","Mondstadt", R.drawable.valberry));
 
                         Log.d("characterLocal name1", String.valueOf(db.getCharacterLocalById(1).getClName()));
 
@@ -171,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
                         db.addCharacterPrimary(new CharacterPrimary("Teachings Of Resistance", "Guide To Resistance", "Philosophies Of Resistance", "domain", R.drawable.teachings_of_resistance, R.drawable.guide_to_resistance, R.drawable.philosophies_of_resistance));
                         db.addCharacterPrimary(new CharacterPrimary("Teachings Of Diligence", "Guide To Diligence", "Philosophies Of Diligence", "domain", R.drawable.teachings_of_diligence, R.drawable.guide_to_diligence, R.drawable.philosophies_of_diligence));
                         db.addCharacterPrimary(new CharacterPrimary("Teachings Of Elegance", "Guide To Elegance", "Philosophies Of Elegance", "domain", R.drawable.teachings_of_elegance, R.drawable.guide_to_elegance, R.drawable.philosophies_of_elegance));
+                        db.addCharacterPrimary(new CharacterPrimary("Teachings Of Ballad", "Guide To Ballad", "Philosophies Of Ballad", "domain", R.drawable.teachings_of_ballad, R.drawable.guide_to_ballad, R.drawable.philosophies_of_ballad));
 
                         Log.d("characterPrimary name1", String.valueOf(db.getCharacterPrimaryById(1).getCpName1()));
 
@@ -178,12 +180,16 @@ public class MainActivity extends AppCompatActivity {
                         db.addCharacterSecondary(new CharacterSecondary("Damaged Mask", "Stained Mask", "Ominous Mask", "wherer", R.drawable.damaged_mask, R.drawable.stained_mask, R.drawable.ominous_mask));
                         db.addCharacterSecondary(new CharacterSecondary("Whopperflower Nectar","Shimmering Nectar","Energy Nectar","wherer",R.drawable.whopperflower_nectar, R.drawable.shimmering_nectar, R.drawable.energy_nectar));
                         db.addCharacterSecondary(new CharacterSecondary("Slime Secretion","Slime Condensate","Slime concentrate","wherer", R.drawable.slime_secretions, R.drawable.slime_condensate, R.drawable.slime_concentrate));
+                        db.addCharacterSecondary(new CharacterSecondary("Recruit's Insignia","Sergeant's Insignia","Lieutenant's Insignia","wherer", R.drawable.recruit_insignia, R.drawable.sergeant_insignia, R.drawable.lieutenant_insignia));
+
                         Log.d("characterSecondary name1", String.valueOf(db.getCharacterSecondaryById(1).getCseName1()));
 
                         db.addCharacterStone(new CharacterStone("Everflame Seed", R.drawable.everflame_seed));
                         db.addCharacterStone(new CharacterStone("Crystalline Bloom", R.drawable.crystalline_bloom));
                         db.addCharacterStone(new CharacterStone("Juvenile Jade", R.drawable.juvenile_jade));
                         db.addCharacterStone(new CharacterStone("Riftborn Regalia", R.drawable.riftborn_regalia));
+                        db.addCharacterStone(new CharacterStone("Hoarfrost Core", R.drawable.hoarfrost_core));
+
                         Log.d("characterStone name", String.valueOf(db.getCharacterStoneById(1).getCstName()));
 
                         db.addWeaponPrimary(new WeaponPrimary("weaqpp", "name2", "name3", "name4", "name5", R.drawable.tile_of_decarabians_tower, R.drawable.debris_of_decarabians_city, R.drawable.fragment_of_decarabians_epic, R.drawable.scattered_piece_of_decarabians_dream));
@@ -197,6 +203,8 @@ public class MainActivity extends AppCompatActivity {
                         db.addWeekly(new Weekly("Dragon Lord's Crown",R.drawable.dragon_lords_crown));
                         db.addWeekly(new Weekly("Shard Of A Foul Legacy", R.drawable.shard_of_a_foul_legacy));
                         db.addWeekly(new Weekly("Ashen Heart", R.drawable.ashen_heart));
+                        db.addWeekly(new Weekly("Shadow of the Warrior", R.drawable.ashen_heart));
+
                         Log.d("weekly name", String.valueOf(db.getWeeklyById(1).getName()));
 
 //                        db.addFavorite(2);
